1ec90f68d7c9b73fd9d06af96d41de693ea05955
Adam Nord <adam.nord@abbgymnasiet.se>
2022-01-19 21:03:00 +0100

    added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="min-h-screen relative">
+  <teleport to="head">
+    <title>ABB Prints</title>
+  </teleport>
+  <div class="min-h-screen relative overflow-hidden pb-24">
+    <transition name="fade">
+      <Sidebar v-if="store.sidebar"></Sidebar>
+    </transition>
     <Header></Header>
     <div class="container mx-auto flex p-4 justify-center">
       <router-view></router-view>
@@ -11,6 +17,10 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import Sidebar from "./components/Sidebar.vue";
+import { useStore } from "./store";
+
+const store = useStore();
 </script>
 
 <style lang="scss">
@@ -19,5 +29,15 @@ body {
 }
 .global-text {
   @apply text-gray-900;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.25s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    ref="target"
+    class="absolute top-0 right-0 bg-abb-500 w-3/4 max-w-sm h-screen z-10"
+  >
+    <div class="flex items-center justify-end w-full p-4">
+      <XIcon @click="store.sidebar = false" class="text-abb-400 h-6"></XIcon>
+    </div>
+    <router-link to="/" @click="sidebar = false">
+      <div
+        class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
+      >
+        Home
+      </div>
+    </router-link>
+    <router-link to="/statistics" @click="sidebar = false">
+      <div
+        class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
+      >
+        Statistics
+      </div>
+    </router-link>
+    <router-link to="/about" @click="sidebar = false">
+      <div
+        class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
+      >
+        About us
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
+import { XIcon } from "@heroicons/vue/outline/index.js";
+import { useStore } from "../store";
+const target = ref(null);
+const store = useStore();
+onClickOutside(target, (event) => (store.sidebar = false));
+</script>
