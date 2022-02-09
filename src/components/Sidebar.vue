04e0af662ffe21f64b567bb7ef060991580ec8cb
Adam Nord <adam.nord@abbgymnasiet.se>
2022-02-09 13:16:27 +0100

    made sidebar nice

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,43 +1,45 @@
 <template>
-  <transition appear name="fade">
+  <transition name="fade">
     <div
       v-if="store.sidebar"
-      class="absolute top-0 w-full h-screen z-10 flex justify-between bg-black/[0.4]"
+      @click="store.sidebar = false"
+      class="absolute top-0 w-full h-screen z-10 bg-black/[0.4]"
+    ></div>
+  </transition>
+  <transition name="slide">
+    <div
+      v-if="store.sidebar"
+      class="h-full absolute right-0 w-3/4 bg-abb-500 max-w-sm z-20"
     >
-      <div @click="store.sidebar = false" class="h-full w-full"></div>
-      <transition appear name="slide">
-        <div class="h-full bg-abb-500 w-full max-w-sm">
-          <div class="flex h-14 items-center justify-end w-full">
-            <button
-              @click="store.sidebar = false"
-              class="h-full w-14 flex items-center justify-center"
-            >
-              <XIcon class="text-abb-400 h-6"></XIcon>
-            </button>
-          </div>
-          <router-link to="/" @click="store.sidebar = false">
-            <div
-              class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
-            >
-              Home
-            </div>
-          </router-link>
-          <router-link to="/statistics" @click="store.sidebar = false">
-            <div
-              class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
-            >
-              Statistics
-            </div>
-          </router-link>
-          <router-link to="/about" @click="store.sidebar = false">
-            <div
-              class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
-            >
-              About us
-            </div>
-          </router-link>
+      <div class="flex h-14 items-center justify-end w-full">
+        <button
+          @click="store.sidebar = false"
+          class="h-full w-14 flex items-center justify-center"
+        >
+          <XIcon class="text-abb-400 h-6"></XIcon>
+        </button>
+      </div>
+      <router-link to="/" @click="store.sidebar = false">
+        <div
+          class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
+        >
+          Home
         </div>
-      </transition>
+      </router-link>
+      <router-link to="/statistics" @click="store.sidebar = false">
+        <div
+          class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
+        >
+          Statistics
+        </div>
+      </router-link>
+      <router-link to="/about" @click="store.sidebar = false">
+        <div
+          class="w-full h-14 flex items-center p-4 border-abb-400 text-abb-400 border-b-2 border-opacity-10"
+        >
+          About us
+        </div>
+      </router-link>
     </div>
   </transition>
 </template>
@@ -51,7 +53,7 @@ const store = useStore();
 <style>
 .slide-enter-active,
 .slide-leave-active {
-  transition: all 0.15s;
+  transition: all 0.25s;
 }
 .slide-enter-from,
 .slide-leave-to {
